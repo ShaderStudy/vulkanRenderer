36a5a8dc71c7bea2dbc420868c3546ac41058194
lmwlmw <leemwymw@naver.com>
2026-04-05 16:24:01 +0900

    Add basic Vulkan triangle renderer example

---
+++ build/CMakeFiles/vulkan_triangle.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vulkan_triangle.dir/src/main.cpp.o"
+  "CMakeFiles/vulkan_triangle.dir/src/main.cpp.o.d"
+  "vulkan_triangle"
+  "vulkan_triangle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vulkan_triangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
